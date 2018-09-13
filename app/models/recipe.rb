e9556195e455147e99c47b2911bde3076d713972
Samantha Helene Thomas <2shthomas@gmail.com>
2018-09-13 10:54:14 -0600

    controllers

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,15 @@
 class Recipe < ApplicationRecord
+
+    #Validations
+    validates_presence_of :name
+
+    #this ensures that we do not have  duplicates of the same named ingredients
+    validates_uniqueness_of: name, case_sensitive:false
+
+    #Associations
+
+    has_many :recipe_ingredients, dependent: :destroy
+    has_many :recpies, through : :recipe_ingredients
+
+
 end
